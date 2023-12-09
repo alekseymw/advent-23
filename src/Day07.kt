e94da32ad3ef9071b51aa0b880775fa2385b0689
alekseim <alekseymw@gmail.com>
2023-12-09 08:16:58 +0100

    Day 07 moved code to main

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,70 +1,71 @@
 import java.util.function.Function
 import kotlin.math.pow
 
-fun part1(input: List<String>): Int {
-    val cardStrength = "23456789TJQKA"
-    val sorted = sortByStrength(input, cardStrength, ::handStrength)
-    return sorted.mapIndexed { index, pair -> pair.second * (index + 1) }
-        .sum()
-}
+fun main() {
 
-fun part2(input: List<String>): Int {
-    val cardStrength = "J23456789TQKA"
-    val sorted = sortByStrength(input, cardStrength, ::handStrengthWithJokers)
-    return sorted.mapIndexed { index, pair -> pair.second * (index + 1) }
-        .sum()
-}
-
-fun handStrength(hand: String): Int {
-    val map = mutableMapOf<Char, Int>()
-    for (card in hand) {
-        map.merge(card, 1, Int::plus)
+    fun handStrength(hand: String): Int {
+        val map = mutableMapOf<Char, Int>()
+        for (card in hand) {
+            map.merge(card, 1, Int::plus)
+        }
+        return map.map { it.value.toDouble().pow(2) }.sum().toInt()
     }
-    return map.map { it.value.toDouble().pow(2) }.sum().toInt()
-}
 
-fun handStrengthWithJokers(hand: String): Int {
-    val map = mutableMapOf<Char, Int>()
-    var j = 0
-    for (card in hand) {
-        if (card == 'J') j++ else map.merge(card, 1, Int::plus)
+    fun handStrengthWithJokers(hand: String): Int {
+        val map = mutableMapOf<Char, Int>()
+        var j = 0
+        for (card in hand) {
+            if (card == 'J') j++ else map.merge(card, 1, Int::plus)
+        }
+        if (map.isEmpty()) map['J'] = j else {
+            val max = map.maxBy { it.value }
+            map[max.key] = max.value + j
+        }
+        return map.map { it.value.toDouble().pow(2) }.sum().toInt()
     }
-    if (map.isEmpty()) map['J'] = j else {
-        val max = map.maxBy { it.value }
-        map[max.key] = max.value + j
-    }
-    return map.map { it.value.toDouble().pow(2) }.sum().toInt()
-}
 
-fun parseInput(input: List<String>): List<Pair<String, Int>> {
-    return input.map {
-        val (hand, bid) = it.split(" ")
-        hand to bid.toInt()
-    }
-}
-
-private fun sortByStrength(
-    input: List<String>,
-    cardStrength: String,
-    handStrengthFunction: Function<String, Int>
-) = parseInput(input).sortedWith { cur, other ->
-    val curStr = handStrengthFunction.apply(cur.first)
-    val otherStr = handStrengthFunction.apply(other.first)
-    var result = 0
-
-    if (curStr != otherStr) result = curStr.compareTo(otherStr)
-    else {
-        for (i in cur.first.indices) if (cardStrength.indexOf(cur.first[i])
-                .compareTo(cardStrength.indexOf(other.first[i])) != 0
-        ) {
-            result = cardStrength.indexOf(cur.first[i]).compareTo(cardStrength.indexOf(other.first[i]))
-            break
+    fun parseInput(input: List<String>): List<Pair<String, Int>> {
+        return input.map {
+            val (hand, bid) = it.split(" ")
+            hand to bid.toInt()
         }
     }
-    result
-}
 
-fun main() {
+    fun sortByStrength(
+        input: List<String>,
+        cardStrength: String,
+        handStrengthFunction: Function<String, Int>
+    ) = parseInput(input).sortedWith { cur, other ->
+        val curStr = handStrengthFunction.apply(cur.first)
+        val otherStr = handStrengthFunction.apply(other.first)
+        var result = 0
+
+        if (curStr != otherStr) result = curStr.compareTo(otherStr)
+        else {
+            for (i in cur.first.indices) if (cardStrength.indexOf(cur.first[i])
+                    .compareTo(cardStrength.indexOf(other.first[i])) != 0
+            ) {
+                result = cardStrength.indexOf(cur.first[i]).compareTo(cardStrength.indexOf(other.first[i]))
+                break
+            }
+        }
+        result
+    }
+
+    fun part1(input: List<String>): Int {
+        val cardStrength = "23456789TJQKA"
+        val sorted = sortByStrength(input, cardStrength, ::handStrength)
+        return sorted.mapIndexed { index, pair -> pair.second * (index + 1) }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val cardStrength = "J23456789TQKA"
+        val sorted = sortByStrength(input, cardStrength, ::handStrengthWithJokers)
+        return sorted.mapIndexed { index, pair -> pair.second * (index + 1) }
+            .sum()
+    }
+
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 6440)
     val testInput2 = readInput("Day07_test2")
